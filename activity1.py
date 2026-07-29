420c29adf87e80821b98fcb90283a648a09f06f3
Uzayer0909 <uzairsayed205@gmail.com>
2026-07-29 20:11:32 +0600

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,19 @@
+list = [154, "python", 555, 444.666, "codingal"]
+
+print(list)
+
+print(len(list))
+
+print (list[1])
+
+print(list[3])
+
+
+print(list[-1])
+
+print(list[2:5])
+
+for item in list:
+    print(item)
+
+print(list[::-1])
